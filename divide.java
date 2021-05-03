9233ab2f0f5987fc062f64c549d1687fef247104
gcurco <58973841+gcurco@users.noreply.github.com>
2021-05-03 15:31:45 +0200

    Add files via upload

---
+++ divide.java
@@ -0,0 +1,13 @@
+public class divide {
+
+    public static void main(String[] args) {
+        
+        double randomize = Math.random();
+
+        if (randomize >= 0.5) {
+            System.out.println(10/1);
+        } else {
+            System.out.println(10/0);
+        }
+    }
+}
